a0b7fcc6d99e8eb5b1e08c40a5065f461aef4ede
i-am-rk <dev.ravikiran.rk@gmail.com>
2020-02-08 22:52:18 +0530

    export mongoConnect

--- util/database.js
+++ util/database.js
@@ -1,12 +1,15 @@
 const mongodb = require('mongodb');
 const MongoClient = mongodb.MongoClient;
 
-const mongoConnect = () =>{
+const mongoConnect = (callback) => {
     MongoClient.connect('mongodb://127.0.0.1:27017/express-cart')
             .then(result =>{
                 console.log('connected!');
+                callback(result);
             })
             .catch(err =>{
                 console.log(err);
             });
 }
+
+module.exports = mongoConnect
