8cf12e4ff6306c793f34d5a10584274de3a3035e
i-am-rk <dev.ravikiran.rk@gmail.com>
2020-02-08 22:49:59 +0530

    mong

--- util/database.js
+++ util/database.js
@@ -1,10 +1,12 @@
 const mongodb = require('mongodb');
 const MongoClient = mongodb.MongoClient;
 
-MongoClient.connect('mongodb://127.0.0.1:27017/express-cart')
+const mongoConnect = () =>{
+    MongoClient.connect('mongodb://127.0.0.1:27017/express-cart')
             .then(result =>{
                 console.log('connected!');
             })
             .catch(err =>{
                 console.log(err);
             });
+}
