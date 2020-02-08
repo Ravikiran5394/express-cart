0ea0d3b34c25f403205a33dfee0fd30583958e94
i-am-rk <dev.ravikiran.rk@gmail.com>
2020-02-08 22:58:29 +0530

    config mongoConnect in app.js

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const mongoConnect = require('./util/database')
+
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -21,4 +23,9 @@ app.use((req, res, next) => {
   res.status(404).render('404', { pageTitle: 'Page Not Found',path:'/404' });
 });
 
-app.listen(3000);
+mongoConnect(client => {
+  console.log(client);
+  app.listen(3000);
+});
+
+
