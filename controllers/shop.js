4d203c48b94d7cee9bf8ab57d0875d82b6d8c1be
i-am-rk <dev.ravikiran.rk@gmail.com>
2020-02-09 11:13:03 +0530

    Index page complete

--- controllers/shop.js
+++ controllers/shop.js
@@ -28,13 +28,17 @@ exports.getProductDetail = (req, res, next) => {
 };
 
 exports.getIndex = (req, res, next) => {
-  Product.fetchAll(products => {
-    res.render("shop/index", {
-      prods: products,
-      pageTitle: "Shop",
-      path: "/"
+  Product.fetchAll()
+    .then(products => {
+      res.render("shop/index", {
+        prods: products,
+        pageTitle: "Shop",
+        path: "/"
+      });
+    })
+    .catch(err => {
+      console.log(err);
     });
-  });
 };
 
 exports.getCart = (req, res, next) => {
