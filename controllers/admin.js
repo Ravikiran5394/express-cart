6bf96b4b1110dc19976f37802ddf95c12415c2fe
i-am-rk <dev.ravikiran.rk@gmail.com>
2020-02-09 11:09:05 +0530

    Fetch All products on /product route

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,50 +1,49 @@
-const Product = require('../modules/product');
+const Product = require("../modules/product");
 
 exports.getAddproduct = (req, res, next) => {
-    res.render('admin/edit-product', {
-      pageTitle: 'Add Product',
-      path: '/admin/add-product',
-      editing:false
-    });
+  res.render("admin/edit-product", {
+    pageTitle: "Add Product",
+    path: "/admin/add-product",
+    editing: false
+  });
 };
 
 exports.postAddProduct = (req, res, next) => {
-    const title = req.body.title;
-    const imageUrl = req.body.imageUrl;
-    const price = req.body.price;
-    const description = req.body.description;
-    const product = new Product(title,price,description,imageUrl);
-    product
-      .save()
-      .then(result =>{
-        console.log('Created Product!');
-        res.redirect('/admin/products')
-      })
-      .catch(err => {
-        if(err){
-          console.log(err);
-        }
-      });
+  const title = req.body.title;
+  const imageUrl = req.body.imageUrl;
+  const price = req.body.price;
+  const description = req.body.description;
+  const product = new Product(title, price, description, imageUrl);
+  product
+    .save()
+    .then(result => {
+      console.log("Created Product!");
+      res.redirect("/admin/products");
+    })
+    .catch(err => {
+      if (err) {
+        console.log(err);
+      }
+    });
 };
 
 exports.getEditProduct = (req, res, next) => {
   const editMode = req.query.edit;
-  if(!editMode){
-    return res.redirect('/')
+  if (!editMode) {
+    return res.redirect("/");
   }
   const prodId = req.params.productId;
-  Product.findById(prodId, product =>{
-    if(!product){
-      return res.redirect('/');
+  Product.findById(prodId, product => {
+    if (!product) {
+      return res.redirect("/");
     }
-    res.render('admin/edit-product', {
-      pageTitle: 'Edit Product',
-      path: '/admin/edit-product',
+    res.render("admin/edit-product", {
+      pageTitle: "Edit Product",
+      path: "/admin/edit-product",
       editing: editMode,
-      product:product
+      product: product
     });
-  })
-  
+  });
 };
 
 exports.postEditProduct = (req, res, next) => {
@@ -54,24 +53,30 @@ exports.postEditProduct = (req, res, next) => {
   const updatedImageUrl = req.body.imageUrl;
   const updatedDescription = req.body.description;
 
-  const updatedProduct = new Product(prodId,updatedTitle,updatedImageUrl,updatedDescription,updatedPrice);
+  const updatedProduct = new Product(
+    prodId,
+    updatedTitle,
+    updatedImageUrl,
+    updatedDescription,
+    updatedPrice
+  );
   updatedProduct.save();
-  res.redirect('/admin/products');
-}
+  res.redirect("/admin/products");
+};
 
-exports.getProducts = (req,res,next) =>{
-    Product.fetchAll((products)=>{
-        res.render('admin/products', {
-          prods: products,
-          pageTitle: 'Products',
-          path: 'admin/products',
-        });
-      });
-}
+exports.getProducts = (req, res, next) => {
+  Product.fetchAll(products => {
+    res.render("admin/products", {
+      prods: products,
+      pageTitle: "Products",
+      path: "admin/products"
+    });
+  });
+};
 
 // /delete-product => POST => controller
-exports.postDeleteProduct = (req,res,next)=>{
+exports.postDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
   Product.deleteById(prodId);
-  res.redirect('/admin/products')
-}
+  res.redirect("/admin/products");
+};
--- modules/product.js
+++ modules/product.js
@@ -1,26 +1,39 @@
-const getDb = require('../util/database').getDb;
+const getDb = require("../util/database").getDb;
 
 class Product {
-    constructor(title,price,description,imageUrl){
-        this.title = title;
-        this.price = price;
-        this.description = description;
-        this.imageUrl = imageUrl;
-    }
-    // Methods =>
-    save(){
-        const db = getDb();
-        return db.collection('products')
-            .insertOne(this)
-            .then(result => {
-                console.log(result.insertedCount + " Product inserted!");
-            })
-            .catch(err =>{
-                console.log(err);
-            });
-    }
-    static fetchAll(){
-
-    }
+  constructor(title, price, description, imageUrl) {
+    this.title = title;
+    this.price = price;
+    this.description = description;
+    this.imageUrl = imageUrl;
+  }
+  // Methods =>
+  save() {
+    const db = getDb();
+    return db
+      .collection("products")
+      .insertOne(this)
+      .then(result => {
+        console.log(result.insertedCount + " Product inserted!");
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+  static fetchAll() {
+    const db = getDb();
+    return db
+      .collection("products")
+      .find()
+      .toArray()
+      .then(products =>{
+          return products;
+      })
+      .catch(err => {
+          if(err){
+              console.log(err)
+          }
+      });
+  }
 }
-module.exports = Product
+module.exports = Product;
