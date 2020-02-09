2ced123a636e161f474686ee1482c89a85879b0a
i-am-rk <dev.ravikiran.rk@gmail.com>
2020-02-09 23:41:37 +0530

    attached user to req

--- app.js
+++ app.js
@@ -1,30 +1,42 @@
-const path = require('path');
+const path = require("path");
 
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
 
-const mongoConnect = require('./util/database').mongoConnect;
+const mongoConnect = require("./util/database").mongoConnect;
 
 const app = express();
 
-app.set('view engine', 'ejs');
-app.set('views', 'views');
+app.set("view engine", "ejs");
+app.set("views", "views");
 
-const adminData = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
+const adminData = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+const User = require("./modules/user");
 
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/admin', adminData.routes);
+// Find User and store it in request
+app.use((req, res, next) => {
+  User.findById('5e404323b6198e1124863d4a')
+    .then(user => {
+      req.user = user;
+      next();
+    })
+    .catch(err => {
+      console.log(err);
+    });
+});
+app.use("/admin", adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).render('404', { pageTitle: 'Page Not Found',path:'/404' });
+  res.status(404).render("404", { pageTitle: "Page Not Found", path: "/404" });
 });
 
 mongoConnect(() => {
-  app.listen(3000,() => {console.log('server listening on 3000')});
+  app.listen(3000, () => {
+    console.log("server listening on 3000");
+  });
 });
-
-
